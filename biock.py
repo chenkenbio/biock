6db15777c587e9a4b595f84d4a5b2f58e72f57dc
chenken <chenkenck@outlook.com>
2020-05-16 17:36:09 +0800

    update model summary

--- biock.py
+++ biock.py
@@ -59,16 +59,42 @@ def run_bash(cmd):
 
 
 ## deep learning related
+#def model_summary(model):
+#    print("model_summary")
+#    print("Layer_name"+"\t"*7+"Number of Parameters")
+#    print("="*100)
+#    model_parameters = [layer for layer in model.parameters() if layer.requires_grad]
+#    layer_name = [child for child in model.children()]
+#    j = 0
+#    total_params = 0
+#    print("\t"*10)
+#    for i in layer_name:
+#        print()
+#        param = 0
+#        try:
+#            bias = (i.bias is not None)
+#        except:
+#            bias = False  
+#        if not bias:
+#            param =model_parameters[j].numel()+model_parameters[j+1].numel()
+#            j = j+2
+#        else:
+#            param =model_parameters[j].numel()
+#            j = j+1
+#        print(str(i)+"\t"*3+str(param))
+#        total_params+=param
+#    print("="*100)
+#    print(f"Total Params:{total_params}")     
+
 def model_summary(model):
     """
     model: pytorch model
     """
+    import torch
     total_param = 0
     trainable_param = 0
-    for p in model.parameters():
-        num_p = 1
-        for n in p.shape:
-            num_p *= n
+    for i, p in enumerate(model.parameters()):
+        num_p = torch.numel(p)
         if p.requires_grad:
             trainable_param += num_p
         total_param += num_p
