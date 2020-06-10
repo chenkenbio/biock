507159a641dd99147667a5b0a7c7bdbfeae20edd
chenken <chenkenck@outlook.com>
2020-06-10 16:09:02 +0800

    add freq_import.py

---
+++ freq_import.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import argparse, os, re, sys, warnings, time, json, gzip
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr, spearmanr
+import pickle
