1a35566b5178bbc515f120b8adf7bca9ec226e9a
chenken <chenkenck@outlook.com>
2020-10-17 19:37:26 +0800

    cal-metrics.py: add F1 score, MCC

--- bin/cal-metrics.py
+++ bin/cal-metrics.py
@@ -3,7 +3,7 @@
 import argparse, os, sys, warnings, time, json, gzip
 import numpy as np
 
-from sklearn.metrics import roc_auc_score, precision_recall_curve, auc, precision_score, recall_score, matthews_corrcoef, f1_score, average_precision_score
+from sklearn.metrics import roc_auc_score, precision_recall_curve, auc, precision_score, recall_score, matthews_corrcoef, f1_score, average_precision_score, matthews_corrcoef
 from scipy.stats import pearsonr, spearmanr
 
 
@@ -60,6 +60,17 @@ def max_f1(y_true, y_prob):
             max_f1 = f1
     return max_f1, best_cutoff
             
+def max_mcc(y_true, y_prob):
+    max_mcc = -1
+    best_cutoff = None
+    for cutoff in np.linspace(min(y_prob) + 1E-6, max(y_prob) - 1E-6, 100):
+        pred = (y_prob >= cutoff).astype(int)
+        mcc = matthews_corrcoef(y_true, pred)
+        if mcc > max_mcc:
+            best_cutoff = cutoff
+            max_mcc = mcc
+    return max_mcc, best_cutoff
+ 
 
 
 def get_args():
@@ -86,28 +97,46 @@ if __name__ == "__main__":
             score.append(float(s))
     label = np.array(label)
     score = np.array(score)
+
+    label_score = zip(list(score), list(label))
+    label_score = sorted(label_score, key=lambda l:l[0], reverse=True)
+
+    
+    n_pos = int(sum(label))
+    top10 = sum([a[1] for a in label_score[0:10]])     / min(n_pos, 10)
+    top100 = sum([a[1] for a in label_score[0:100]])   / min(n_pos, 100)
+    top200 = sum([a[1] for a in label_score[0:200]])   / min(n_pos, 200)
+    top500 = sum([a[1] for a in label_score[0:500]])   / min(n_pos, 500)
+    top1000 = sum([a[1] for a in label_score[0:1000]]) / min(n_pos, 1000)
+
     AUC = roc_auc_score(label, score)
     AP = average_precision_score(label, score)
     AUPR = cal_aupr(label, score)[0]
     print("# {}".format(args.file))
-    print("# label count: {}".format(label_count(label)))
+    print("# label count: {}".format(label_count(label)).replace(',', ' ,').replace('(', '( ').replace(')', ' )'))
     print("- AUC:  {:.5f}".format(AUC))
     print("- AUPR: {:.5f}".format(AUPR))
     print("- AP:   {:.5f}".format(AP))
     if args.f1_cutoff is None:
         F1, cutoff = max_f1(label, score)
         args.f1_cutoff = cutoff
-        print("F1 cutoff (max F1): {}".format(args.f1_cutoff))
-    else:
-        print("F1 cutoff (pre-defined): {}".format(args.f1_cutoff))
     pred = (score > args.f1_cutoff).astype(int)
     N = len(pred)
     F1 = f1_score(label, pred)
-    print("- F1:   {:.5f}".format(F1))
+    print("\n- F1:   {:.5f} (cutoff = {:.5f})".format(F1, args.f1_cutoff).replace(')', ' )'))
     FDR = np.logical_and(label == 0, pred == 1).sum() / np.sum(pred)
     FOR = np.logical_and(label == 1, pred == 0).sum() / (N - np.sum(pred))
-    print("- FDR:  {:.5f}".format(FDR))
-    print("- FOR:  {:.5f}".format(FOR))
+    print(" - FDR:  {:.5f}".format(FDR))
+    print(" - FOR:  {:.5f}\n".format(FOR))
+
+    mcc, mcc_cutoff = max_mcc(label, score)
+    print("- MCC: {:.5f} (cutoff = {:.5f})\n".format(mcc, mcc_cutoff).replace(')', ' )'))
+
+    print("- Top10   ACC: {:.1f}".format(top10))
+    print("- Top100  ACC: {:.2f}".format(top100))
+    print("- Top200  ACC: {:.2f}".format(top200))
+    print("- Top500  ACC: {:.3f}".format(top500))
+    print("- Top1000 ACC: {:.3f}".format(top1000))
 
     #mean_FDR, mean_FOR = average_FDR_FOR(label, score)
     #print("- Average FDR: {:.5f}".format(mean_FDR))
