22d9ccb2dbd493cb3b0e5d5b335399371315cc6c
ken <chenkenck@outlook.com>
2021-04-02 00:34:05 +0800

    add option "--no-chr"

--- bin/sort_bed.sh
+++ bin/sort_bed.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "usage: bed"
+    echo "usage: bed [--no-chr]"
     exit 1
 fi
 
 bed="$1"
+no_chr="$2"
+if [ "$no_chr" = "--no-chr" ]; then
+    sed 's/^chr//;s/^X/23/;s/^Y/24/;s/^MT/25/;s/^M/25/' $bed | sort -k1,1n -k2,2n -k3,3n | sed 's/^23\t/X\t/; s/^24\t/Y\t/; s/^25\t/M\t/'
+else
+    sed 's/^chr//;s/^X/23/;s/^Y/24/;s/^MT/25/;s/^M/25/' $bed | sort -k1,1n -k2,2n -k3,3n | sed 's/^23\t/X\t/; s/^24\t/Y\t/; s/^25\t/M\t/;s/^/chr/'
+fi
 
-sed 's/^chr//;s/^X/23/;s/^Y/24/;s/^MT/25/;s/^M/25/' $bed | sort -k1,1n -k2,2n -k3,3n | sed 's/^23\t/X\t/; s/^24\t/Y\t/; s/^25\t/M\t/;s/^/chr/'
