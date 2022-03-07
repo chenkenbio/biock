65ee63b7046f871739bd80296951e0155b8231bc
chenkenbio <chenkenbio@gmail.com>
2022-03-08 00:30:27 +0800

    add: KL-divergence, onehot array maker

--- pytorch/pytorch_utils.py
+++ pytorch/pytorch_utils.py
@@ -2,6 +2,9 @@
 
 import torch
 import torch.nn as nn
+from torch import Tensor
+import numpy as np
+from typing import Literal, Optional
 
 ## genome one hot
 # 'N': 0
@@ -24,4 +27,19 @@ def model_summary(model):
             trainable_param += num_p
         total_param += num_p
     return {'total_param': total_param, 'trainable_param': trainable_param}
+
+
+def make_onehot(N: int, dtype=torch.float) -> Tensor:
+    return torch.as_tensor(np.diag(np.ones(N)), dtype=dtype)
+
+
+def kl_divergence(mu1: Tensor, logvar1: Tensor, mu2: Optional[Tensor]=None, logvar2: Optional[Tensor]=None, reduction: Literal["mean"]="mean") -> Tensor:
+    if mu2 is None:
+        mu2 = torch.zeros_like(mu1)
+        logvar2 = torch.zeros_like(logvar1)
+    kl_div = 0.5 * (logvar2 - logvar1 + (logvar1.exp() + (mu1 - mu2).pow(2)) / logvar2.exp() - 1)
+    kl_div = kl_div.mean(dim=1)
+    if reduction == "mean":
+        kl_div = torch.mean(kl_div)
+    return kl_div
 
