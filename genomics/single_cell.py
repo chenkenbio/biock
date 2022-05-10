dd5cccdfff35ba2adcf97ae0ae984aa559bf8630
chenkenbio <chenkenbio@gmail.com>
2022-05-10 21:04:05 +0800

    update

--- genomics/single_cell.py
+++ genomics/single_cell.py
@@ -4,13 +4,15 @@ Author: Ken Chen (chenkenbio@gmail.com)
 Date: <<date>>
 """
 
-from typing import List, Optional
+from typing import Any, List, Optional, Union, Iterable, Literal, Dict
 import scanpy as sc
 from anndata import AnnData
 import scipy.sparse as ssp
 import pandas as pd
+from pandas import Index
+from sklearn.feature_extraction.text import TfidfTransformer
 import gzip
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, issparse
 import numpy as np
 
 from ..logger import make_logger
@@ -27,8 +29,8 @@ def keep_common_cells(*args: AnnData) -> List[AnnData]:
     print(len(index))
     return [a[index, ] for a in args]
 
-def random_adata(nrow: int, ncol: int, density: float=0.001) -> AnnData:
-    return AnnData(X=ssp.random(nrow, ncol, density=0.001, format="csr", dtype=np.float32))
+def random_adata(nrow: int, ncol: int, density: float=0.001, dtype=np.float32) -> AnnData:
+    return AnnData(X=ssp.random(nrow, ncol, density=density, format="csr", dtype=dtype), dtype=dtype)
 
 
 ## convert anndata to mtx.gz, features.tsv.gz and barcodes.tsv.gz
@@ -71,3 +73,60 @@ def adata2mtx(adata: AnnData, prefix: str, use_rep: str):
         obs=adata.obs,
         var=adata.var
     )
+
+def fix_peak_index(peaks: Iterable[str]) -> Index:
+    r"""
+    Parameters
+    -----------
+    peaks : AnnData.var.index
+        (["chr1_100_200", ...]/["chr1\t100\t200", ...]/["chr1:100-200", ...])
+    
+    Return
+    -------
+    peaks : peak names in standard format (chr1:100-200)
+    """
+    p = peaks[0]
+    if '\t' in p:
+        sep = '\t'
+    elif '_' in p:
+        sep = '_'
+    else:
+        sep = None
+    if sep is not None:
+        new_peaks = list()
+        for p in peaks:
+            c, s, e = p.split(sep)
+            new_peaks.append("{}:{}-{}".format(c, s, e))
+        return new_peaks
+    else:
+        return peaks
+
+def tfidf_transform(
+        sparse_matrix: csr_matrix, 
+        norm: Literal["l2", "l1", None]=None, 
+        binary :bool=True, 
+        get_model: bool=False) -> csr_matrix:
+    if binary:
+        sparse_matrix.data = np.ones_like(sparse_matrix.data, dtype=np.float32)
+    tfidf = TfidfTransformer(norm=norm)
+    sparse_matrix = tfidf.fit_transform(sparse_matrix)
+    if get_model:
+        return sparse_matrix, tfidf
+    else:
+        return sparse_matrix
+
+
+def stat_adata(adata: AnnData) -> Dict[str, Any]:
+    if isinstance(adata.X, np.ndarray):
+        d = {
+            "shape": adata.shape,
+            "X-min/mean/max": (adata.X.min(), adata.X.mean(), adata.X.max()),
+        }
+    else:
+        d = {
+            "shape": adata.shape,
+            "density": round(len(adata.X.data) / np.prod(adata.X.shape), 6),
+            "X-min/max": (adata.X.min(), adata.X.max()),
+            "X-min/mean/max(nonzero)": (round(adata.X.data.min(), 4), round(adata.X.data.mean(), 4), round(adata.X.data.max(), 4))
+        }
+    return d
