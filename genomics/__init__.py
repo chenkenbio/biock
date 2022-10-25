d1dd2804fef099c730721934a9d8eddc13dc6706
chenkenbio <chenkenbio@gmail.com>
2022-10-25 20:43:47 +0800

    add variables of genomes

--- genomics/__init__.py
+++ genomics/__init__.py
@@ -1,42 +1,172 @@
 #!/usr/bin/env python3
 
-import os
+import os, json
+from .toolbox import parse_gtf_record, chrom_remove_chr, ensembl_remove_version, chrom_add_chr, fix_peak_name, load_gene_info
+import numpy as np
+from typing import Any, Dict, Iterable, List, Literal, Optional, Union
+from scipy.sparse import issparse, csr_matrix
+# from .single_cell import keep_common_cells, random_adata
+import warnings
 
 HOME = os.environ["HOME"]
 
-HG19_CHROMSIZE = {"chr1": 249250621, "chr2": 243199373, 
-        "chr3": 198022430, "chr4": 191154276, 
-        "chr5": 180915260, "chr6": 171115067, 
-        "chr7": 159138663, "chr8": 146364022, 
-        "chr9": 141213431, "chr10": 135534747, 
-        "chr11": 135006516, "chr12": 133851895, 
-        "chr13": 115169878, "chr14": 107349540, 
-        "chr15": 102531392, "chr16": 90354753, 
-        "chr17": 81195210, "chr18": 78077248, 
-        "chr19": 59128983, "chr20": 63025520, 
-        "chr21": 48129895, "chr22": 51304566, 
-        "chrX": 155270560, "chrY": 59373566,
-        "chrM": 16569, "chrMT": 16569}
-HG38_CHROMSIZE = {"chr1": 248956422, "chr2": 242193529,
-        "chr3": 198295559, "chr4": 190214555,
-        "chr5": 181538259, "chr6": 170805979,
-        "chr7": 159345973, "chr8": 145138636,
-        "chr9": 138394717, "chr10": 133797422,
-        "chr11": 135086622, "chr12": 133275309,
-        "chr13": 114364328, "chr14": 107043718,
-        "chr15": 101991189, "chr16": 90338345,
-        "chr17": 83257441, "chr18": 80373285,
-        "chr19": 58617616, "chr20": 64444167,
-        "chr21": 46709983, "chr22": 50818468,
-        "chrX": 156040895, "chrY": 57227415,
-        "chrM": 16569, "chrMT": 16569}
-CHROM_SIZE_DICT = {'hg19': HG19_CHROMSIZE, 'GRCh37': HG19_CHROMSIZE, "hg38": HG38_CHROMSIZE, "GRCh38": HG38_CHROMSIZE}
+def _counts_per_size(mtx: np.ndarray, log: bool=False, target_reads: int=1e6) -> np.ndarray:
+    """
+    Args:
+        mtx : cell by gene matrix
+    Return:
+        cpm/logcpm
+    """
+    size = np.asarray(mtx).sum(axis=1)
+    cpm = ((target_reads / size) * mtx.T).T
+    if log:
+        cpm = np.log1p(cpm)
+    return cpm
 
+def counts_per_thousand(mtx, log: bool=False) -> np.ndarray:
+    return _counts_per_size(mtx, log, target_reads=1000)
+def counts_per_million(mtx, log: bool=False) -> np.ndarray:
+    return _counts_per_size(mtx, log, target_reads=1E6)
+
+def _reverse_counts_per_scale(mtx, libsize, target_reads) -> np.ndarray:
+    raise NotImplementedError
+
+def reverse_counts_per_million():
+    raise NotImplementedError
+    return _reverse_counts_per_scale()
+
+
+__chrom_sizes__ = json.load(open(os.path.join(os.path.dirname(__file__), "chrom_sizes.json")))
+
+HG19_CHROMSIZE = __chrom_sizes__["HG19_CHROMSIZE"]
+HG38_CHROMSIZE = __chrom_sizes__["HG38_CHROMSIZE"]
+MM10_CHROMSIZE = __chrom_sizes__["MM10_CHROMSIZE"]
+del __chrom_sizes__
+
+CHROM_SIZE_DICT = {
+        'hg19': HG19_CHROMSIZE, 'GRCh37': HG19_CHROMSIZE, 
+        "hg38": HG38_CHROMSIZE, "GRCh38": HG38_CHROMSIZE,
+        "hg38-shuffle": HG38_CHROMSIZE, 
+        "hg38-random": HG38_CHROMSIZE, 
+        "mm10": MM10_CHROMSIZE
+    }
 
 HG19_FASTA = os.path.join(HOME, "db/gencode/GRCh37/GRCh37.primary_assembly.genome.fa")
 HG38_FASTA = os.path.join(HOME, "db/gencode/GRCh38/GRCh38.primary_assembly.genome.fa")
-FASTA_DICT = {'hg19': HG19_FASTA, 'GRCh37': HG19_FASTA, 'hg38': HG38_FASTA, 'GRCh38': HG38_FASTA}
+MM9_FASTA = os.path.join(HOME, "db/UCSC/mm9/mm9.fa")
+MM10_FASTA = os.path.join(HOME, "db/UCSC/mm10/mm10.fa")
+MM39_FASTA = os.path.join(HOME, "db/gencode/GRCm39/GRCm39.primary_assembly.genome.fa")
 
-from .transcripts import load_transcripts, Transcript, AnnotatedTranscript
 
-from .toolbox import onehot_dna_rna
+HG19_SIZE_FN = HG19_FASTA + ".chromsize"
+HG38_SIZE_FN = HG38_FASTA + ".chromsize"
+MM39_SIZE_FN = MM39_FASTA + ".chromsize"
+
+HG19_FASTA_INT8 = os.path.join(HOME, "db/gencode/GRCh37/GRCh37.primary_assembly.genome.int8.pkl")
+HG38_FASTA_INT8 = os.path.join(HOME, "db/gencode/GRCh38/GRCh38.primary_assembly.genome.int8.pkl")
+# MM10_FASTA_INT8 = os.path.join(HOME, "db/gencode/GRCm38/GRCm38.primary_assembly.genome.int8.pkl")
+MM10_FASTA_INT8 = os.path.join(HOME, "db/UCSC/mm10/mm10.int8.pkl")
+MM9_FASTA_INT8  = os.path.join(HOME, "db/UCSC/mm9/mm9.int8.pkl")
+
+
+FASTA_DICT = {
+        'hg19': HG19_FASTA, 'GRCh37': HG19_FASTA, 
+        'hg38': HG38_FASTA, 'GRCh38': HG38_FASTA,
+        "mm10": MM10_FASTA
+    }
+
+FASTA_INT8_DICT = {
+        'hg19': HG19_FASTA_INT8, 'GRCh37': HG19_FASTA_INT8, 
+        'hg38': HG38_FASTA_INT8, 'GRCh38': HG38_FASTA_INT8,
+        "mm10": MM10_FASTA_INT8
+    }
+
+
+_CHROM2INT = {
+    "chr1": 1,   "1": 1,   1: 1,
+    "chr2": 2,   "2": 2,   2: 2,
+    "chr3": 3,   "3": 3,   3: 3,
+    "chr4": 4,   "4": 4,   4: 4,
+    "chr5": 5,   "5": 5,   5: 5,
+    "chr6": 6,   "6": 6,   6: 6,
+    "chr7": 7,   "7": 7,   7: 7,
+    "chr8": 8,   "8": 8,   8: 8,
+    "chr9": 9,   "9": 9,   9: 9,
+    "chr10": 10, "10": 10, 10: 10,
+    "chr11": 11, "11": 11, 11: 11,
+    "chr12": 12, "12": 12, 12: 12,
+    "chr13": 13, "13": 13, 13: 13,
+    "chr14": 14, "14": 14, 14: 14,
+    "chr15": 15, "15": 15, 15: 15,
+    "chr16": 16, "16": 16, 16: 16,
+    "chr17": 17, "17": 17, 17: 17,
+    "chr18": 18, "18": 18, 18: 18,
+    "chr19": 19, "19": 19, 19: 19,
+    "chr20": 20, "20": 20, 20: 20,
+    "chr21": 21, "21": 21, 21: 21,
+    "chr22": 22, "22": 22, 22: 22,
+    "chrX": 23,  "X": 23,
+    "chrY": 24,  "Y": 24,
+    "chrM": 25,  "M": 25
+}
+class Chrom2Int(object):
+    def __init__(self) -> None:
+        self.mapping = _CHROM2INT.copy()
+        self.reverse_mapping = {v:k for k, v in self.mapping.items()}
+        self._next = max(self.mapping.values()) + 1
+
+    def __call__(self, chrom) -> int:
+        if chrom not in self.mapping:
+            self.mapping[chrom] = self._next
+            self.reverse_mapping[self._next] = chrom
+            self._next += 1
+        return self.mapping[chrom]
+    
+    def int2chrom(self, idx):
+        return self.reverse_mapping[idx]
+        
+
+def chrom2int(chrom):
+    if chrom in _CHROM2INT:
+        chrom = _CHROM2INT[chrom]
+    return chrom
+
+NN_COMPLEMENT = {
+    'A': 'T', 'a': 't',
+    'C': 'G', 'c': 'g',
+    'G': 'C', 'g': 'c',
+    'T': 'A', 't': 'a',
+    'R': 'Y', 'r': 'y',
+    'Y': 'R', 'y': 'r',
+    'S': 'S', 's': 's',
+    'W': 'W', 'w': 'w',
+    'K': 'M', 'k': 'm',
+    'M': 'K', 'm': 'k',
+    'B': 'V', 'b': 'v',
+    'D': 'H', 'd': 'h',
+    'H': 'D', 'h': 'd',
+    'V': 'B', 'v': 'b',
+    'N': 'N', 'n': 'n'
+}
+
+def get_reverse_strand(seq, join: bool=True):
+    if join:
+        seq = ''.join([NN_COMPLEMENT.get(n, n) for n in seq[::-1]])
+    else:
+        seq = [NN_COMPLEMENT.get(n, n) for n in seq[::-1]]
+    return seq
+
+
+# from .transcripts import load_transcripts, Transcript, AnnotatedTranscript
+
+
+def load_chrom_size(fai) -> Dict[str, int]:
+    chrom_size = dict()
+    with open(fai) as infile:
+        for l in infile:
+            chrom, size = l.split()[:2]
+            chrom_size[chrom] = int(size)
+    # if chrom.startswith("chr"):
+    #     chrom_size[chrom[3:]] = int(size)
+    return chrom_size
+
--- genomics/toolbox.py
+++ genomics/toolbox.py
@@ -3,19 +3,252 @@
 Author: Ken Chen (chenkenbio@gmail.com)
 Date: <<date>>
 """
+import os
+from tqdm import tqdm
+import warnings
+import sys
+from collections import defaultdict, namedtuple, OrderedDict
+from typing import Any, Union, List, Dict, Tuple
+import json
+import logging
+import pickle
+from ..utils import copen, has_module
+from ..variables import HUMAN_CHROMS_ALL
+try:
+    import pyfaidx
+except ImportError as err:
+    warnings.warn("Missing module: pyfaidx\n{}".format(err))
 
-from ..biock import nt_onehot_dict
-import numpy as np
+logger = logging.getLogger(__name__)
 
 
-def onehot_dna_rna(seq):
-    return np.array([nt_onehot_dict[n] for n in seq])
-# import logging, warnings, json, gzip, pickle
-# from collections import defaultdict, OrderedDict
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# from scipy.stats import pearsonr, spearmanr, ttest_ind, mannwhitneyu
-# from sklearn.metrics import auc, roc_auc_score, roc_curve, precision_recall_curve, average_precision_score
+GTFRecord = namedtuple("GTF_record", field_names=["chrom", "start", "end", "strand", "feature_type", "attrs"])
+def parse_gtf_record(gtf_line: str, to_dict=False) -> GTFRecord:
+    r"""
+    Return:
+    -------
+    chrom : str
+    ftype : str
+    start : int
+    end : int
+    strand : str
+    attrs : dict
+    """
+    try:
+        chrom, _, ftype, start, end, _, strand, _, fields = gtf_line.strip().rstrip(';').split('\t')
+    except:
+        raise RuntimeError("{}".format(gtf_line.strip()))
+    attrs = dict()
+    for kv in fields.split('; '):
+        try:
+            v = kv.split(' ')
+            k = v[0]
+            v = ' '.join(v[1:])
+        except ValueError as err:
+            print(kv, gtf_line)
+            exit(err)
+        v = v.strip('"')
+        if k in attrs:
+            if isinstance(attrs[k], str):
+                attrs[k] = set([attrs[k]])
+            attrs[k].add(v)
+        else:
+            attrs[k] = v
+    if to_dict:
+        return {
+                    "chrom":  chrom, 
+                    "ftype": ftype, 
+                    "start": int(start), 
+                    "end": int(end), 
+                    "strand": strand, 
+                    "attrs": attrs
+                }
+    else:
+        return GTFRecord(chrom=chrom, start=int(start), end=int(end), strand=strand, feature_type=ftype, attrs=attrs)
+
+
+def chrom_add_chr(chrom: Union[str, List[str]]) -> Union[str, List[str]]:
+    if isinstance(chrom, str):
+        chrom = "chr{}".format(chrom) if not chrom.startswith("chr") else chrom
+    else:
+        chrom = ['chr{}'.format(c) if not c.startswith("chr") else c for c in chrom]
+    return chrom
+
+def chrom_remove_chr(chrom: Union[str, List[str]]) -> Union[str, List[str]]:
+    if isinstance(chrom, str):
+        chrom = chrom.replace("chr", '')
+    else:
+        chrom = [c.replace("chr", '') for c in chrom]
+    return chrom
+
+
+def chrom_add_chr(chrom: str) -> str:
+    if isinstance(chrom, str):
+        chrom = chrom if chrom.startswith("chr") else "chr{}".format(chrom)
+    else:
+        chrom = [chrom if chrom.startswith("chr") else "chr{}".format(chrom) for c in chrom]
+    return chrom
+
+def ensembl_remove_version(ensembl_id: str) -> str:
+    if ensembl_id.startswith("ENS"):
+        suffix = ""
+        if ensembl_id.endswith("_PAR_Y"):
+            suffix = "_PAR_Y"
+        ensembl_id = ensembl_id.split('.')[0] + suffix
+    return ensembl_id
+        
+def fix_peak_name(name: str, sep: str) -> str:
+    c, s, e = name.split(sep)
+    return "{}:{}-{}".format(c, s, e)
+
+
+## gene & peaks
+def load_gene_info(tss_bed, prefix=None) -> Tuple[Dict, Dict, Dict, Dict]:
+    """
+    Return
+    -------
+    gene2chrom : Dict[str, List[str]], {"ENSGxxx": ["chr1", "chr2", ...], ...}
+    gene2tss : Dict[str, Tuple[str, int]], {"ENSGxxx": [["chr1": 100], ["chr1": 102], ...], ...}
+    gene_id2name : Dict[str, List[str]], 
+    gene_name2id :
+    """
+    all_loaded = True if prefix is not None else False
+    if prefix is not None:
+        try:
+            gene2chrom = json.load(open("{}.gene2chrom.json".format(prefix)))
+            gene2tss = json.load(open("{}.gene2tss.json".format(prefix)))
+            gene_id2name = json.load(open("{}.gene_id2name.json".format(prefix)))
+            gene_name2id = json.load(open("{}.gene_name2id.json".format(prefix)))
+            logger.info("- logging cache from {}.*.json".format(prefix))
+        except FileNotFoundError:
+            all_loaded = False
+    
+    if not all_loaded:
+        gene2tss, gene2chrom = defaultdict(set), defaultdict(set)
+        gene_name2id, gene_id2name = defaultdict(set), defaultdict(set)
+        non_chrom_contigs = set()
+        with open(tss_bed) as infile:
+            for l in infile:
+                chrom, _, tss, name, _, strand = l.strip().split('\t')
+                non_chrom_contigs.add(chrom)
+                # if chrom not in HUMAN_CHROMS_NO_MT:
+                #     continue
+                gene_id, gene_name, gene_type, tx_id, tss, strand = name.split('|')
+                gene_id = ensembl_remove_version(gene_id)
+                tss = int(tss)
+                gene2tss[gene_id].add((chrom, tss, strand))
+                gene2tss[gene_name].add((chrom, tss, strand))
+                gene2chrom[gene_id].add(chrom)
+                gene2chrom[gene_name].add(chrom)
+
+                gene_name2id[gene_name].add(gene_id)
+                gene_id2name[gene_id].add(gene_name)
+        non_chrom_contigs = non_chrom_contigs.difference(HUMAN_CHROMS_ALL)
+
+        gene2chrom = dict(gene2chrom)
+        gene2tss = dict(gene2tss)
+        gene_name2id = dict(gene_name2id)
+        gene_id2name = dict(gene_id2name)
+
+        for g, chroms in gene2chrom.items():
+            if len(chroms) > 1 and 'chrY' in chroms:
+                chroms.remove('chrY')
+            if len(chroms.difference(non_chrom_contigs)) > 0:
+                chroms = chroms.difference(non_chrom_contigs)
+            tss = list()
+            for c, t, strand in gene2tss[g]:
+                if c in chroms:
+                    tss.append([c, t, strand])
+            gene2tss[g] = tss
+            gene2chrom[g] = list(chroms)
+
+        gene_name2id = {g: list(v) for g, v in gene_name2id.items()}
+        gene_id2name = {g: list(v) for g, v in gene_id2name.items()}
+
+        if prefix is not None:
+            json.dump(gene2chrom, open("{}.gene2chrom.json".format(prefix), 'w'), indent=4) 
+            json.dump(gene2tss, open("{}.gene2tss.json".format(prefix), 'w'), indent=4)
+            json.dump(gene_id2name, open("{}.gene_id2name.json".format(prefix), 'w'), indent=4)
+            json.dump(gene_name2id, open("{}.gene_name2id.json".format(prefix), 'w'), indent=4)
+    return gene2chrom, gene2tss, gene_id2name, gene_name2id
 
 
 
+def load_fasta(fn: str, no_chr: bool=False, ordered: bool=False, cache: bool=True, gencode_style: bool=False) -> Dict[str, str]:
+    r"""
+    load fasta as sequence dict
+    Input
+    -----
+    fn : path to fasta file
+    ordered : False - dict, True - OrderedDict
+    gencode_style : seq_id|xxxxxx
+
+    Return
+    -------
+    seq_dict : Dict[str, str] or OrderedDict[str, str]
+    """
+    # if fn == "GRCh38":
+    #     fn = HG38_FASTA
+    #     logger.warning("- using {}".format(fn))
+    #     cache = True
+    # elif fn == "GRCh37":
+    #     fn = HG19_FASTA
+    #     logger.warning("- using {}".format(fn))
+    #     cache = True
+
+    if ordered:
+        fasta = OrderedDict()
+    else:
+        fasta = dict()
+    name, seq = None, list()
+    if cache:
+        if no_chr:
+            cache = fn + (".gencode.nochr.cache.pkl" if gencode_style else ".nochr.cache.pkl")
+        else:
+            cache = fn + (".gencode.cache.pkl" if gencode_style else ".cache.pkl")
+    else:
+        cache = None
+    if cache is not None and os.path.exists(cache):
+        # logger.info("- load processed genome: {}".format(cache))
+        logger.warning("- load processed genome: {}".format(cache))
+        fasta = pickle.load(open(cache, 'rb'))
+    else:
+        with copen(fn) as infile:
+            for l in infile:
+                if l.startswith('>'):
+                    if name is not None:
+                        # print("{}\n{}".format(name, ''.join(seq)))
+                        if no_chr:
+                            name = name.replace("chr", '')
+                        fasta[name] = ''.join(seq)
+                    if gencode_style:
+                        name = l.strip().lstrip('>').split('|')[0]
+                    else:
+                        name = l.strip().lstrip('>').split()[0]
+                    seq = list()
+                else:
+                    seq.append(l.strip())
+        fasta[name] = ''.join(seq)
+        if cache is not None:
+            try:
+                pickle.dump(fasta, open(cache, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+            except IOError as err:
+                warnings.warn("{}".format(err))
+    return fasta
+
+
+class Genome(object):
+    def __init__(self, fasta, in_memory: bool=True) -> None:
+        self.in_memory = in_memory
+        if in_memory and has_module("pyfaidx"):
+            self.fasta = pyfaidx.Fasta(fasta)
+        else:
+            self.fasta = load_fasta(fasta)
+    
+    def get_seq(self, chrom: str, start: int, end: int) -> str:
+        seq = self.fasta[chrom][start:end]
+        if self.in_memory:
+            seq = seq.seq
+        return seq
+        
+        
