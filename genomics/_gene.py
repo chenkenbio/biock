bb03d012a173617dd3a2370cf59d02f2d39e2504
chenkenbio <chenkenbio@gmail.com>
2022-11-24 16:59:00 +0800

    fix bugs

--- genomics/_gene.py
+++ genomics/_gene.py
@@ -13,23 +13,23 @@ import pandas as pd
 from typing import Any, Dict, Iterable, List, Literal, Optional, Tuple, Union
 
 class Transcript(object):
-    tx_id: str
-    gene_id: str
-    chrom: str
-    tx_start: int
-    tx_end: int
-    strand: str
-    exon_starts: List[int]
-    exon_ends: List[int]
 
-    cds_start: int=None
-    cds_end: int=None
-    buildver: str=None
-    gene_name: str=None
-    cds: List[Tuple[int]]=None
-
-    def __init__(self) -> None:
-        raise NotImplementedError
+    def __init__(self, tx_id, gene_id, chrom, tx_start, tx_end, strand, cds_start=None, cds_end=None, buildver=None, gene_name=None, **kwargs) -> None:
+        self.tx_id = tx_id
+        self.gene_id = gene_id
+        self.chrom = chrom
+        self.tx_start = tx_start
+        self.tx_end = tx_end
+        self.strand = strand
+        self.cds_start = cds_start
+        self.cds_end = cds_end
+        self.buildver = buildver
+        self.gene_name = gene_name
+        self._cds: List[Tuple[int, int]]=list()
+        self._utr5: List[Tuple[int, int]]=list()
+        self._utr3: List[Tuple[int, int]]=list()
+        for k, v in kwargs.items():
+            self.__dict__[k] = v
 
     def __repr__(self) -> str:
         repr = list()
@@ -48,7 +48,83 @@ class Transcript(object):
         if not hasattr(self, "_intron_ends"):
             self._intron_ends = self.exon_starts[1:]
         return self._intron_ends
+
+    @property
+    def intron(self):
+        return list(zip(self.intron_starts, self.intron_ends))
+
+    @property
+    def exon(self):
+        return list(zip(self.exon_starts, self.exon_ends))
     
+    def _get_utr_cds(self):
+        assert self.cds_start is not None and self.cds_end is not None, "the start/end of CDS is unknown"
+        if len(self._utr5) == 0 and len(self._cds) == 0 and len(self._utr3) == 0:
+            utr_left, utr_right = list(), list()
+            for e1, e2 in zip(self.exon_starts, self.exon_ends):
+                left, right = max(e1, self.cds_start), min(e2, self.cds_end)
+                if left < right:
+                    self._cds.append((left, right))
+                if e1 < self.cds_start:
+                    utr_left.append((e1, min(e2, self.cds_start)))
+                if e2 > self.cds_end:
+                    utr_right.append((max(self.cds_end, e1), e2))
+            if self.strand == '+':
+                self._utr5 = utr_left
+                self._utr3 = utr_right
+            else:
+                self._utr5 = utr_right
+                self._utr3 = utr_left
+    
+    def check(self):
+        # check cds&utr
+        exon_size, cds_size, utr_size = 0, 0, 0
+        if self.strand == '+':
+            for x, y in self.utr5:
+                utr_size += y - x
+                assert x < y, "{}".format(((x, y), self.cds_start, self.cds_end))
+                assert y <= self.cds_start, "{}".format(((x, y), self.cds_start, self.cds_end))
+            for x, y in self.utr3:
+                utr_size += y - x
+                assert x < y, "{}".format(((x, y), self.cds_start, self.cds_end))
+                assert x >= self.cds_end, "{}".format(((x, y), self.cds_start, self.cds_end))
+        else:
+            for x, y in self.utr5:
+                utr_size += y - x
+                assert x < y, "{}".format(((x, y), self.cds_start, self.cds_end))
+                assert x >= self.cds_end, "{}".format(((x, y), self.cds_start, self.cds_end))
+            for x, y in self.utr3:
+                utr_size += y - x
+                assert x < y, "{}".format(((x, y), self.cds_start, self.cds_end))
+                assert y <= self.cds_start, "{}".format(((x, y), self.cds_start, self.cds_end))
+        for x, y in self.cds:
+            assert x >= self.cds_start, "{}".format(((x, y), self.cds_start, self.cds_end))
+            assert y <= self.cds_end, "{}".format(((x, y), self.cds_start, self.cds_end))
+            cds_size += y - x
+        exon_size = sum([y - x for x, y in zip(self.exon_starts, self.exon_ends)])
+        assert exon_size == cds_size + utr_size, "{}".format((exon_size, utr_size, cds_size))
+        intervals = sorted(t.utr5 + t.utr3 + t.cds + t.intron, key=lambda x:x[0])
+        for i in range(len(intervals) - 1):
+            assert intervals[i][1] == intervals[i +  1][0], ';'.join(["({},{})".format(x, y) for x, y in intervals])
+
+    @property
+    def cds(self):
+        if len(self._cds) == 0:
+            self._get_utr_cds()
+        return self._cds
+
+    @property
+    def utr5(self):
+        if len(self._utr5) == 0:
+            self._get_utr_cds()
+        return self._utr5
+
+    @property
+    def utr3(self):
+        if len(self._utr3) == 0:
+            self._get_utr_cds()
+        return self._utr3
+
     def get_function_region(self, function_region: Literal["exon", "intron", "ss-exon", "ss-intron-1", "ss-intron-2"], name: List[str]='.', name_sep: str='|', numbering: bool=False) -> List[Tuple[str, int, int, str, str, str]]:
         r"""
         Arguments:
