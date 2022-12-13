d5f273215790bd4661961a30b74d8559ef655663
chenkenbio <chenkenbio@gmail.com>
2022-12-13 21:09:17 +0800

    minor changes

--- bin/cal-metrics.py
+++ bin/cal-metrics.py
@@ -10,25 +10,28 @@ from sklearn.metrics import (
     roc_curve, precision_recall_curve
 )
 
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import biock
-from biock import auto_open
+from biock import auto_open, count_items
 from biock.ml_tools import topk_acc_1d, pr_auc_score
 from scipy.stats import pearsonr, spearmanr
 
 
+
 def get_args():
     p = argparse.ArgumentParser()
-    p.add_argument('-i', '--input', help="label\tscore\t...", nargs='+')
+    p.add_argument('-i', '--input', help="label\tscore\t...", nargs='+', required=True)
     p.add_argument('-t', "--task", choices=("C", "R", "classification", "regression"), help="classification(C)/regression(R) metrics", default="classification")
     p.add_argument("-m", "--metrics", nargs='+', choices=("AUC", "AP", "F1", "topK-ACC", "PCC", "SCC", "MSE"))
     p.add_argument('-c', '--cutoff', default=0.5, type=float, help="cutoff used in F1/MCC calculation")
     p.add_argument('-r', "--reverse", action='store_true', help="reverse score")
-    p.add_argument("-l", "--label-column", default=0, help="column ID of label (0-start)")
-    p.add_argument("-s", "--score-column", default=1, help="column ID of score (0-start)")
+    p.add_argument("-l", "--label-column", default=0, help="column ID of label (0-start)", type=int)
+    p.add_argument("-s", "--score-column", default=1, help="column ID of score (0-start)", type=int)
+    p.add_argument("--skip-labels", type=str, nargs='+')
     p.add_argument("--skiprows", default=0, type=int)
     p.add_argument("--comment", default="#", type=str)
     p.add_argument('-d', "--delimiter", default='\t')
+    p.add_argument("--allow-nan", action="store_true")
     p.add_argument("--single-line", action="store_true")
     # p.add_argument('-roc', action='store_true', help="save fpr(X)/tpr(Y) for plotting ROC curve")
     # p.add_argument('-pr', action='store_true', help="save recall(X)/precision(Y) for plotting ROC curve")
@@ -41,24 +44,38 @@ if __name__ == "__main__":
 
     label, score = list(), list()
 
+    skip_labels = dict()
+    if args.skip_labels is not None:
+        for k in args.skip_labels:
+            skip_labels[k] = 0
+
+
     for fn in args.input:
         with auto_open(fn) as infile:
-            for nr, l in enumerate(infile):
-                if nr < args.skiprows or l.startswith(args.comment):
+            for nr, line in enumerate(infile):
+                if nr < args.skiprows or line.startswith(args.comment):
                     continue
-                fields = l.strip("\n").split(args.delimiter)
+                fields = line.strip("\n").split(args.delimiter)
                 l, s = fields[args.label_column], fields[args.score_column]
+                if l in skip_labels:
+                    skip_labels[l] += 1
                 label.append(float(l))
                 score.append(float(s))
     label = np.asarray(label).astype(int)
     score = np.asarray(score)
+    nan_scores = None
+    if args.allow_nan:
+        keep = ~np.isnan(score)
+        nan_scores = count_items(label[~keep], fraction=True)
+        label = label[keep]
+        score = score[keep]
     if args.reverse:
         score = -score
 
     results = OrderedDict()
     classification_metrics = ["AUC", "AP", "AUPR", "topK-ACC", "F1", "MCC", "precision", "recall", "ACC"]
     if args.task == "classification" or args.task == "C":
-        assert len(np.unique(label)) == 2
+        assert len(np.unique(label)) == 2, "{}".format(np.unique(label, return_counts=True))
         if args.metrics is None:
             args.metrics = classification_metrics
         else:
@@ -87,7 +104,10 @@ if __name__ == "__main__":
                 raise ValueError("unknown metric {}".format(m))
 
         print(biock.get_run_info(sys.argv, args))
-        print("#label count: {}".format(biock.count_items(label)).replace(',', ' ,').replace('(', '( ').replace(')', ' )'))
+        print("#labels skipped: {}".format(skip_labels))
+        if nan_scores is not None:
+            print("#nan scores skipped: {}".format(nan_scores))
+        print("#label count: {}".format(biock.count_items(label, fraction=True)).replace(',', ' ,').replace('(', '( ').replace(')', ' )'))
         if args.single_line:
             print('#' + '\t'.join([x for x in results.keys()]))
             print('\t'.join(["{:.4f}".format(x) for x in results.values()]))
