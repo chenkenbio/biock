a73dbfcf8b0e5633b44d5c67a4fecdc1102ec042
chenkenbio <chenkenbio@gmail.com>
2022-12-27 01:43:59 +0800

    update

--- pytorch/__init__.py
+++ pytorch/__init__.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+import warnings
 
 from .pytorch_utils import *
-from .attention import SelfAttentionEmbedding, PerformerEncoder, PerformerEncoderLayer
+from .attention import SelfAttentionEmbedding # , PerformerEncoder, PerformerEncoderLayer, PositionalEncoding
+from .cnn_modules import build_cnn1d
+from .mlp_modules import build_mlp
+try:
+    from .dgl_gnn_modules import *
+except ImportError as err:
+    warnings.warn("{}".format(err))
