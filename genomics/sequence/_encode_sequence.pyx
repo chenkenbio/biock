1d82df92b99ef830f59c0e97eddaf5c0de63fc81
chenkenbio <chenkenbio@gmail.com>
2022-12-27 01:34:16 +0800

    add sequence encoder

---
+++ genomics/sequence/_encode_sequence.pyx
@@ -0,0 +1,24 @@
+# cython: language_level=3
+
+cimport cython
+
+NN_DICT = {
+    'N': 0, 'n': 0,
+    'A': 1, 'a': 1,
+    'C': 2, 'c': 2,
+    'G': 3, 'g': 3,
+    'T': 4, 't': 4,
+    'U': 4, 'u': 4,
+}
+
+@cython.boundscheck(False)
+@cython.wraparound(False) 
+def _encode_sequence(str seq, dict nn_dict=NN_DICT):
+    cdef list ids = list()
+    cdef int seq_len = len(seq)
+
+    for i in range(seq_len):
+        ids.append(nn_dict[seq[i]])
+    return ids
+
+## compile: python compile_cython.py build_ext --inplace
--- genomics/sequence/bioseq_tokenizer.py
+++ genomics/sequence/bioseq_tokenizer.py
@@ -14,9 +14,10 @@ from typing import Any, Dict, Iterable, List, Literal, Optional, Tuple, Union
 import logging
 logger = logging.getLogger(__name__)
 
-from _sequence_to_kmer_list import _fast_seq_to_kmer_list
+from ._sequence_to_kmer_list import _fast_seq_to_kmer_list
+from ._encode_sequence import _encode_sequence
 
-def fast_seq_to_kmer_list(text: str, k=1, pad: bool=False, shift: int=None):
+def fast_seq_to_kmer_list(text: str, k=1, pad: bool=False, shift: int=None) -> List[str]:
     if shift is None:
         shift = 1
     if k == 1:
@@ -27,6 +28,14 @@ def fast_seq_to_kmer_list(text: str, k=1, pad: bool=False, shift: int=None):
             return _fast_seq_to_kmer_list(text, k, k//2, k, shift)
         else:
             return _fast_seq_to_kmer_list(text, k, 0, k, shift)
+
+def encode_sequence(seq: str, token_dict: Dict[str, int]=None) -> List[int]:
+    if token_dict is None:
+        ids = _encode_sequence(seq)
+    else:
+        ids = _encode_sequence(seq, token_dict)
+    return ids
+    
 
 
 def seq_to_kmer_list(seq, k):
@@ -39,3 +48,13 @@ def seq_to_kmer_list(seq, k):
         start = i - pad
         kmers.append(pad_seq[start:start + k])
     return kmers
+
+_NN2INDEX = {n: i for i, n in enumerate(list("NACGT"))}
+_NN2INDEX['U'] = 4
+for n in "NACGTU":
+    _NN2INDEX[n.lower()] = _NN2INDEX[n]
+
+_ONEHOT = np.concatenate((np.ones((1, 4)), np.diag(np.ones(4))))
+def onehot_dna(seq):
+    seq = np.asarray([_NN2INDEX[n] for n in seq])
+    return _ONEHOT[seq]
