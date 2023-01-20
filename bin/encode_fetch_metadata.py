a7d05c9def797cf8f5ebe3f754ed8b59f648545e
chenkenbio <chenkenbio@gmail.com>
2023-01-21 02:40:16 +0800

    fix minor bug: to stdout

--- bin/encode_fetch_metadata.py
+++ bin/encode_fetch_metadata.py
@@ -14,9 +14,9 @@ from subprocess import Popen, PIPE
 def get_args():
     p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('id')
-    p.add_argument("-t", "--type", default="files")
-    p.add_argument("-f", "--force", action="store_true")
-    p.add_argument("-o", "--output")
+    p.add_argument("-t", "--type", default="files", help="ID type, files/experiments/...")
+    p.add_argument("-f", "--force", action="store_true", help="overwrite existing file")
+    p.add_argument("-o", "--output", help="default: stdout")
     p.add_argument('--seed', type=int, default=2020)
     return p
 
@@ -26,15 +26,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.output is None:
-        args.output = args.id + ".json"
-
-    if os.path.exists(args.output) and not args.force:
-        print("{} exists.".format(args.output))
+        args.output = sys.stdout
     else:
-        p = Popen(['/bin/bash', '-c', 'curl -L -H "Accept: application/json" https://www.encodeproject.org/{}/{}/'.format(args.type, args.id)], stdout=PIPE, stderr=PIPE)
-        out, err = p.communicate()
-        if p.returncode != 0:
-            raise RuntimeError(err.decode("utf8"))
-        out = json.load(io.StringIO(out.decode("utf8")))
-        json.dump(out, open(args.output, 'w'), indent=4)
+        if os.path.exists(args.output) and not args.force:
+            print("{} exists.".format(args.output))
+            exit(0)
+        args.output = open(args.output, 'w')
+
+    p = Popen(['/bin/bash', '-c', 'curl -L -H "Accept: application/json" https://www.encodeproject.org/{}/{}/'.format(args.type, args.id)], stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    if p.returncode != 0:
+        raise RuntimeError(err.decode("utf8"))
+    out = json.load(io.StringIO(out.decode("utf8")))
+    json.dump(out, args.output, indent=4)
 
